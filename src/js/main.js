32fc156351f8cca03696b529ab19ab3aa386963a
Nicke Bergman <nickenickebergman@gmail.com>
2023-11-08 14:37:00 +0100

    fdns

--- src/js/main.js
+++ src/js/main.js
@@ -2,31 +2,52 @@ import './../css/style.css'
 
 const appContainer = document.getElementById("app");
 
-const rightNumber = Math.round(Math.random()*100);
-let yourNumber = -1;
+//Uppgift ett GISSA TALET
 
-console.log(rightNumber);
-console.log(yourNumber);
-
+//Skapa html
 const instruction = document.createElement("h1");
 instruction.innerHTML = "Gissa ett tal från 0-100 och tryck på enter";
 appContainer.appendChild(instruction);
 
-const input = document.createElement("input");
-appContainer.appendChild(input);
+const rightNumberInputLabel = document.createElement("label");
+rightNumberInputLabel.innerHTML = "Skriv en hemlig siffra";
+appContainer.appendChild(rightNumberInputLabel);
+
+const rightNumberInput = document.createElement("input");
+rightNumberInput.type = "password";
+appContainer.appendChild(rightNumberInput);
+
+const playerInputLabel = document.createElement("label");
+playerInputLabel.innerHTML = "Gissa siffran";
+appContainer.appendChild(playerInputLabel);
+
+const playerInput = document.createElement("input");
+appContainer.appendChild(playerInput);
 
 const rubrik = document.createElement("h2");
 appContainer.appendChild(rubrik);
 
+//Skapa nummer
+let rightNumber = Number(rightNumberInput.value);
+let yourNumber = -1;
 
-input.addEventListener("keypress", (e) => {
+rightNumberInput.addEventListener("keypress", (e) => {
   if(e.key === "Enter") {
-    yourNumber = Number(input.value);
+    rightNumber = Number(rightNumberInput.value);
+    console.log(rightNumber);
+  }
+})
+
+//Lyssna på gissning
+playerInput.addEventListener("keypress", (e) => {
+  if(e.key === "Enter") {
+    yourNumber = Number(playerInput.value);
     console.log(yourNumber);
     compareNumber();
   }
 });
 
+//Jämför gissning
 function compareNumber() {
   if (yourNumber === rightNumber) {
     rubrik.innerHTML = "Du gissade rätt!";
@@ -39,12 +60,83 @@ function compareNumber() {
   }
 }
 
-
+//Första försöket
 // while(rightNumber !== yourNumber) {
 //   yourNumber = Number(prompt("Skriv ditt tal!"));
   
 //   console.log(yourNumber);
 // }
 
+
+
+
+
+
+//LOOPA LISTA + KLICK
+
+const clothes = ["skor", "klänningar", "jackor", "kjolar"];
+
+const ul = document.createElement("ul");
+
+appContainer.appendChild(ul);
+
+
+// skapa alla li
+
+clothes.forEach(element => {
+  const li = document.createElement("li");
+  li.innerHTML = element;
+  ul.appendChild(li);
+    li.addEventListener("click", () => {
+        clothesSelection(element);
+    })
+
+});
+
+const clothesSelection = (product) => {
+    console.log(product);
+}
+
+
+
+
+//Uppgift tre!
+
+const apiContainer = document.getElementById("api");
+
+async function logMovies() {
+  const response = await fetch("https://medieinstitutet-wie-products.azurewebsites.net/api/products");
+  const movies = await response.json();
+  console.log(movies);
+  // console.log(movies[0].description);
+
+  for (let i = 0; i < movies.length; i++) {
+    const movieCard = document.createElement("article");
+    movieCard.className = "moviecard";
+    apiContainer.appendChild(movieCard);
+
+    const name = document.createElement("h2");
+    name.innerHTML = movies[i].name;
+    movieCard.appendChild(name);
+
+    const img = document.createElement("img");
+    img.alt = "Poster for " + movies[i].name;
+    img.src = movies[i].imageUrl;
+    img.className = "movieposter";
+    movieCard.appendChild(img);
+
+    const description = document.createElement("h2");
+    description.innerHTML = movies[i].description;
+    movieCard.appendChild(description);
+    
+
+
+  }
+  
+  
+  
+}
+
+const filmLista = logMovies();
 
 
